22f7e2232db0caa7db89de03e8802c2e667c2092
Zalzur <tolah94@gmail.com>
2018-11-29 10:52:41 +0100

    Implemented RSSFeed model

---
+++ RSSFeedReader/Controllers/RSSFeedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RSSFeedReader.Controllers
+{
+    public class RSSFeedController : Controller
+    {
+        // GET: RSSFeed
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- RSSFeedReader/Models/RSSFeed.cs
+++ RSSFeedReader/Models/RSSFeed.cs
@@ -7,5 +7,9 @@ namespace RSSFeedReader.Models
 {
     public class RSSFeed
     {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public string PubDate { get; set; }
     }
 }
