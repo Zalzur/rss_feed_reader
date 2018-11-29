53b61b36cfbd22db6ffa8f60a75e2e56bffb5df5
Zalzur <tolah94@gmail.com>
2018-11-29 11:04:30 +0100

    Implemented RSSFeedReader controller

--- RSSFeedReader/Controllers/RSSFeedController.cs
+++ RSSFeedReader/Controllers/RSSFeedController.cs
@@ -1,16 +1,37 @@
-﻿using System;
+﻿using RSSFeedReader.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace RSSFeedReader.Controllers
 {
     public class RSSFeedController : Controller
     {
-        // GET: RSSFeed
         public ActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string RSSURL)
+        {
+            WebClient wclient = new WebClient();
+            string RSSData = wclient.DownloadString(RSSURL);
+
+            XDocument xml = XDocument.Parse(RSSData);
+            var RSSFeedData = (from x in xml.Descendants("item")
+                               select new RSSFeed
+                               {
+                                   Title = (string)x.Element("title"),
+                                   Link = (string)x.Element("link"),
+                                   Description = (string)x.Element("description"),
+                                   PubDate = (string)x.Element("pubDate")
+                               });
+            ViewBag.RSSFeed = RSSFeedData;
+            ViewBag.URL = RSSURL;
             return View();
         }
     }
