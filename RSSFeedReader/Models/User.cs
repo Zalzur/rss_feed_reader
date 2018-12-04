bf91c3ebe5d08afe9c0d87a5d145de0c7b27761c
unknown <tolah94@gmail.com>
2018-12-04 16:49:50 +0100

    Added error handling to user model (with annotations)

--- RSSFeedReader/Models/User.cs
+++ RSSFeedReader/Models/User.cs
@@ -12,14 +12,24 @@ namespace RSSFeedReader.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class User
     {
         public int UserID { get; set; }
+
+        [Required(ErrorMessage = "This field is required!")]
         public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "This field is required!")]
         public string Password { get; set; }
+
         [DisplayName("Confirm Password")]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
+
         public string RSS_Feed { get; set; }
     }
 }
