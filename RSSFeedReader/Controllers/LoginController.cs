95bc33a045a381bdfbac33bf2effc0c5c8c88071
unknown <tolah94@gmail.com>
2018-12-04 19:18:23 +0100

    Implemented Login Controller and View for it

--- RSSFeedReader/Controllers/LoginController.cs
+++ RSSFeedReader/Controllers/LoginController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RSSFeedReader.Models;
 
 namespace RSSFeedReader.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(User userModel)
+        {
+            using (UserModels user = new UserModels())
+            {
+                var userDetails = user.Users.Where(x => x.Username == userModel.Username && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    ViewBag.LoginErrorMessage = "Wrong username or password!";
+                    return View("Login", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.UserID;
+                    return RedirectToAction("Index", "RSSFeed");
+                }
+            }
         }
     }
 }
