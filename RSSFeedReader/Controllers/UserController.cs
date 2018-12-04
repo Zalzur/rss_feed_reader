bae23c56ebad26535099dad6f22447d9a33b143d
unknown <tolah94@gmail.com>
2018-12-04 17:04:41 +0100

    Implemented the connection between view - controller - database

--- RSSFeedReader/Controllers/UserController.cs
+++ RSSFeedReader/Controllers/UserController.cs
@@ -10,10 +10,24 @@ namespace RSSFeedReader.Controllers
     public class UserController : Controller
     {
         // GET: User
+        [HttpGet]
         public ActionResult AddOrEdit(int id=0)
         {
             User userModel = new User();
             return View(userModel);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(User userModel)
+        {
+            using (UserModels user = new UserModels())
+            {
+                user.Users.Add(userModel);
+                user.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration succsessful!";
+            return View("AddOrEdit", new User());
         }
     }
 }
