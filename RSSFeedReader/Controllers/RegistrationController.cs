d08071a4e89bc682492a86e23a0d41f6cf179976
unknown <tolah94@gmail.com>
2018-12-04 22:04:19 +0100

    Renamed controller and view which works with registration

--- RSSFeedReader/Controllers/RegistrationController.cs
+++ RSSFeedReader/Controllers/RegistrationController.cs
@@ -7,32 +7,32 @@ using RSSFeedReader.Models;
 
 namespace RSSFeedReader.Controllers
 {
-    public class UserController : Controller
+    public class RegistrationController : Controller
     {
         // GET: User
         [HttpGet]
-        public ActionResult AddOrEdit(int id=0)
+        public ActionResult Registration(int id=0)
         {
             User userModel = new User();
             return View(userModel);
         }
 
         [HttpPost]
-        public ActionResult AddOrEdit(User userModel)
+        public ActionResult Registration(User userModel)
         {
             using (UserModels user = new UserModels())
             {
                 if (user.Users.Any( x => x.Username == userModel.Username))
                 {
                     ViewBag.DuplicateMessage = "Username already exist!";
-                    return View("AddOrEdit", userModel);
+                    return View("Registration", userModel);
                 }
                 user.Users.Add(userModel);
                 user.SaveChanges();
             }
             ModelState.Clear();
             ViewBag.SuccessMessage = "Registration succsessful!";
-            return View("AddOrEdit", new User());
+            return View("Registration", new User());
         }
     }
 }
